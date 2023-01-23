2d339d91c2001c543da56fc37e2631c256bcc1bc
semccaff <stevenmccaffrey@outlook.com>
2023-01-22 20:35:41 -0700

    webmap upload

---
+++ layers/OilHub_3.js
@@ -0,0 +1,10 @@
+var json_OilHub_3 = {
+"type": "FeatureCollection",
+"name": "OilHub_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Location": "Hardisty, AB" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.274315618699603, 52.640812975489958 ] ] } },
+{ "type": "Feature", "properties": { "id": "2", "Location": "Cushing, OK" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -96.740371014996938, 35.942233471661801 ] ] } },
+{ "type": "Feature", "properties": { "id": "3", "Location": "Houston, TX" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -95.370457409276327, 29.75581067546776 ] ] } }
+]
+}
